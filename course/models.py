92af7b9e5a7322a98c8a198691f356efc4a3b739
smrha <lord.smrha@gmail.com>
2020-08-28 23:07:22 +0430

    Create Course model

--- course/models.py
+++ course/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Course(models.Model):
+    STATUS_CHOICES = (
+        ('پیش نویس', 'پیش نویس'),
+        ('منتشر شده', 'منتشر شده'),
+    )    ]
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextFiled()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=16, choices=STATUS_CHOICES, default='پیش نویس')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
